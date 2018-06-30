ea2af8fc4ef54fcc4f75022a996f160a743d0246
GalbPires <37561564+GalbPires@users.noreply.github.com>
2018-06-29 21:15:29 -0300

    Modelo Final

--- Trabalho PSW1/src/edu/udc/psw/modelo/Modelo.java
+++ Trabalho PSW1/src/edu/udc/psw/modelo/Modelo.java
@@ -1,5 +1,24 @@
 package edu.udc.psw.modelo;
 
 public class Modelo {
+    public static Class<?> PONTO = Ponto2D.class;
+    public static Class<?> LINHA = Linha.class;
+    public static Class<?> TRIANGULO = Triangulo.class;
+    public static Class<?> RETANGULO = Retangulo.class;
+    public static Class<?> POLIGONO = Poligono.class;
+   
+
+   public static Modelo novoModelo(Class<?> modeloClass) {
+    	if(modeloClass.asSubclass(Modelo.class) == null)
+    		throw new IllegalArgumentException("Classe desconhecida: " + modeloClass.getSimpleName());
+    	
+        try {
+            Modelo modelo = (Modelo) modeloClass.newInstance();
+            return modelo;
+        }
+        catch (Exception ex) {
+            throw new RuntimeException("Nao pode instanciar o Modelo: " + modeloClass, ex);
+        }
+    }
 
 }
